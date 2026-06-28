a90f1b6038d4a6774f6621f9b3c63bf0465fe06c
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2026-06-28 20:22:15 +0530

    Remove animations and preloader also for better lcp achievement

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,15 +45,6 @@
     });
   });
 
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
 
   /**
    * Scroll top button
